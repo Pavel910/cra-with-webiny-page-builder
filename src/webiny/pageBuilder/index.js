4bf83851240de412fd9650ffe2184bdb5621f0b3
Pavel Denisjuk <pavel@webiny.com>
2020-02-14 19:28:37 +0100

    Working example

---
+++ src/webiny/pageBuilder/index.js
@@ -0,0 +1,2 @@
+export { default as GenericErrorPage } from "./GenericErrorPage";
+export { default as GenericNotFoundPage } from "./GenericNotFoundPage";
---
+++ src/webiny/plugins.js
@@ -0,0 +1,6 @@
+import pageBuilderPlugins from "@webiny/app-page-builder/site/plugins";
+import pageBuilderTheme from "@webiny/app-page-builder-theme";
+
+const plugins = [pageBuilderPlugins, pageBuilderTheme()];
+
+export default plugins;
